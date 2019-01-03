b9b4f0d8cc36e156c57eb32e5f770e737d724c2a
FRAMGIA\doan.quang.hoa <doan.quang.hoa@framgia.com>
2019-01-03 14:57:25 +0700

    commit

---
+++ src/main/java/com/framgia/demo/FramgiaApplication.java
@@ -0,0 +1,14 @@
+package com.framgia.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FramgiaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FramgiaApplication.class, args);
+    }
+
+}
+
